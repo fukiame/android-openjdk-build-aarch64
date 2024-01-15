fe9304383ac739423cd91100039e7a888aec7a68
fukiame <fukiame@proton.me>
2024-01-15 22:11:18 +0700

    fixup!setdevkitpath: [[[

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -46,7 +46,7 @@ else
 export JVM_PLATFORM=linux
 # Set NDK
 export API=21
-if [[[ -z "$ANDROID_NDK_ROOT" ]]]; then
+if [[ -z "$ANDROID_NDK_ROOT" ]]; then
   export NDK=$PWD/android-ndk-$NDK_VERSION
   export ANDROID_NDK_ROOT=$NDK
 else
