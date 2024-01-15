8414e70170408dc2e3c3259c846b74bc705ea475
fukiame <fukiame@proton.me>
2024-01-15 14:31:17 +0700

    [DNM]ci_build_arch_aarch64: bash -x

--- ci_build_arch_aarch64.sh
+++ ci_build_arch_aarch64.sh
@@ -4,5 +4,5 @@ export TARGET=aarch64-linux-android
 export TARGET_JDK=aarch64
 export NDK_PREBUILT_ARCH=/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/aarch64-linux-android/bin/strip
 
-bash ci_build_global.sh
+bash -x ci_build_global.sh
 
